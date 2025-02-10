c3ae3b2499216ce1e3f137663f8cac381db021d6
yundevingV <ggdbstjd0222@gmail.com>
2025-02-10 13:45:23 +0900

    feat : 삭제 기능 추가

--- practice/vote-app.sol
+++ practice/vote-app.sol
@@ -11,15 +11,16 @@ contract Vote {
     }
 
     // 변수
-    bool live; // 투표가 끝난지 안끝난지
+    bool live; 
     address owner;
-    candidate[] public candidateList; // 후보 리스트
+    candidate[] public candidateList;
 
     // 한 번만 투표가능하도록
     mapping (address => bool) Voted;
 
     // 투표 이벤트
     event AddCandidate(string name);
+    event DeleteCandidate(uint index);
     event UpVote (string candidate, uint upVote);
     event FinishVote(bool live);
     event Voting(address owner);
@@ -38,8 +39,12 @@ contract Vote {
         emit Voting(owner);
     }
 
+    function isOwner() public view returns (bool) {
+        return msg.sender == owner; 
+    }
+
     function addCandidate(string memory _name) public onlyOwner{
-       require (live  == true ); // 투표가 진행중일때만 실행 
+        require (live  == true ); // 투표가 진행중일때만 실행 
         require (candidateList.length < 5); // 후보자는 다섯명까지만
         
         candidateList.push(candidate(_name, 0)); // 후보자 이름과, 투표수 (후보자등록할대는 0표)를 리스트에 푸쉬
@@ -48,23 +53,37 @@ contract Vote {
         emit AddCandidate(_name); // 후보가 추가됨을 이벤트에 알림
     }
     
-    // 투표
-    function upVote(uint _indexOfCandidate) public {
+    function deleteCandidate(uint  _index) public onlyOwner {
         require(live == true);
-        require(_indexOfCandidate < candidateList.length);
+        require(_index < candidateList.length);
+
+        // 마지막 요소로 덮어쓰기
+        candidateList[_index] = candidateList[candidateList.length - 1];
+        candidateList.pop(); // 마지막 요소 제거
+
+        emit DeleteCandidate(_index); // 후보가 삭제됨을 이벤트에 알림
+    }
+
+    // 투표
+    function upVote(uint _index) public {
+        require(live == true);
+        require(_index < candidateList.length);
         require(Voted[msg.sender] == false);
-        candidateList[_indexOfCandidate].upVote++;
+        candidateList[_index].upVote++;
 
         Voted[msg.sender] == true;
 
-        emit UpVote(candidateList[_indexOfCandidate].name, candidateList[_indexOfCandidate].upVote);
+        emit UpVote(candidateList[_index].name, candidateList[_index].upVote);
     }
+
+
 
     // 모든 후보자의 투표 결과를 반환하는 함수
     function getAllCandidates() public view returns (candidate[] memory) {
         return candidateList; // 후보자 리스트를 반환
     }
 
+    // 투표 종료하기
     function finishVote() public {
         require (live  == true ); // 투표가 진행중이면 
         live = false;
